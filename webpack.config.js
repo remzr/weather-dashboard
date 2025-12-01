0f48657bd441094667dcc415cbbc2cdd2dd862b7
remzr <110097561+remzr@users.noreply.github.com>
2025-12-01 21:54:13 +0100

    Setup webpack with HTML, CSS, Image and server module

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        publicPath: "/img", /* to specify image location for html , website URL in production */
+        clean: true,
+    },
+    devtool: "eval-source-map",
+    devServer: {
+        watchFiles: ["./src/template.html"],
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: "./src/template.html",
+    }),],
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpg)$/i,
+                use: ["file-loader"],
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+};
